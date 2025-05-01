6aeab943d821619ff89b90bebfedfdc95a637c81
Ghosth <ghleomyre@gmail.com>
2025-05-01 22:49:47 +0300

    app central

---
+++ TourOperateurBack/urls.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    
+    # Documentation API
+    path('api/schema/', SpectacularAPIView.as_view(), name='schema'),
+    path('api/docs/', SpectacularSwaggerView.as_view(url_name='schema'), name='swagger-ui'),
+    path('api/redoc/', SpectacularRedocView.as_view(url_name='schema'), name='redoc'),
+    
+    # API endpoints
+    path('api/auth/', include('users.urls')),
+    path('api/voyages/', include('voyages.urls')),
+    path('api/reservations/', include('reservations.urls')),
+    path('api/finances/', include('finances.urls')),
+    path('api/analytics/', include('analytics.urls')),
+    path('api/recommandations/', include('recommandations.urls')),
+    path('api/carte/', include('carte.urls')),
+    path('api/insights/', include('insights.urls')),
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
